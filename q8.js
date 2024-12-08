f648a7fbc43a8440547d3cd9498d8729c255d460
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-08 14:21:37 -0300

    Create q8.js

---
+++ q8.js
@@ -0,0 +1,11 @@
+var c = 1
+var mais18 = 0
+while ( c <= 20) {
+    var idade = parseInt (prompt ("informe sua idade"))
+    if ( idade >= 18){
+        mais18++;
+    }
+    c++
+}
+
+    console.log(mais18 + "pessoas são maoires de idade")
